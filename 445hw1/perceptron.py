9207bc1dd8962a1614d409640515f62e94815fb5
Casey Beach <mcsmash@GlaDOS.(none)>
2012-01-19 01:00:10 -0800

    first commit, perceptron.py will read the optdigit files, parse them and convert to lists of integers.  TODO: convert the lists to arrays/matrices.

---
+++ 445hw1/perceptron.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python
+
+import csv
+import random
+import numpy 
+import scipy
+
+class perceptron:
+    
+    learning_rate = 0.2
+    features = 64
+    training_set = []
+    training_set_array = None
+    training_class = []
+    weights = []
+
+    def __init__(self, learning_rate, feature_count, file_name):
+        random.seed()
+
+        self.learning_rate = learning_rate
+        self.feature = feature_count
+        self.training_set = self.get_training_data(file_name)
+        self.weights = [random.random() for i in range(0,feature_count+1)]
+     
+    def get_training_data(self, file_name):
+        file_reader = csv.reader(open(file_name, 'r'))
+        temp_val = []
+        for i in file_reader:
+            temp_val.append(i)
+
+        temp_ints = [map(int, i) for i in temp_val]
+        
+        self.training_set = temp_ints
+        for i in temp_ints:
+            self.training_class.append(i[-1])
+            i[-1] = 1 
+    
+    def cost_function(self, x, y):
+        return 1/2 * (x-y) * (x-y)
+    
+    def gradient_step(self, cost):
+        pass
+
+    def make_array(self, x, y):
+        return array(training_set)
+    
+    def compute():
+               
+
+
+
+
+
+
+    
+if __name__ == "__main__":
+    per = perceptron(0.2, 64, "optdigits.tra")
